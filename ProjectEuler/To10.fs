149acbe72ffc315c93e4dc33a0cec94df678c18a
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-03 16:35:00 +0900

    末尾再帰最適化を考慮してないProblem2

--- ProjectEuler/To10.fs
+++ ProjectEuler/To10.fs
@@ -6,6 +6,7 @@ let Problem1 :int =
     [1..999] |> List.filter(fun x -> x % 3 = 0 || x % 5 = 0 ) |> List.sum
 
 let Problem2 :int = 
+    // たぶん、3n-1項のみを出力して計算したほうがクレバー
     let rec FibList (acc1, acc2) = [
         let v = acc1 + acc2
         if 4_000_000 < v then ()
@@ -14,6 +15,16 @@ let Problem2 :int =
             yield! FibList(acc2, v)
     ]
 
+    FibList (0, 1)
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.sum
+
+let Problem2_1 :int = 
+    // 末尾再帰なしのfib数列
+    let rec FibList (acc1, acc2) = 
+        if 4_000_000 < acc2 then []
+        else acc2 :: FibList (acc2, acc1 + acc2) 
+    
     FibList (0, 1)
     |> List.filter (fun x -> x % 2 = 0)
     |> List.sum
