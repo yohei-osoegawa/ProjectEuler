8323a73311bce393ba21127be01aa28dac5062c3
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-03 17:26:21 +0900

    till Problem4

--- ProjectEuler/To10.fs
+++ ProjectEuler/To10.fs
@@ -36,5 +36,18 @@ let Problem3 =
         elif num % div = 0L then div :: pf (num / div) div
         else pf num (div + 1L)
     pf given 2L |> List.last
+
+let Problem4 =
+    let cand =
+        (seq{999..-1..0}, seq{999..-1..0}) ||> Seq.allPairs
+        |> Seq.sortByDescending (fun (x,y) -> x * y)
+        |> Seq.find ( fun (x, y) -> 
+            let str = string (x * y)
+            let revStr = str.ToCharArray() |> Array.rev |>fun x -> new System.String(x)
+            str = revStr)
+        |> fun (x,y) -> x*y
+    
+    0
+
     
         
