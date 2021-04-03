798bd8c886a2e65114fe5610076b1a4fb85f54f3
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-03 15:20:53 +0900

    Problem2まで

--- ProjectEuler/Program.fs
+++ ProjectEuler/Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open To10
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    stdout.WriteLine(To10.Problem1)
+    stdout.WriteLine(To10.Problem2)
+    0
---
+++ ProjectEuler/To10.fs
@@ -0,0 +1,19 @@
+﻿module To10
+
+open Microsoft.FSharp.Core.Operators.Checked
+
+let Problem1 :int =
+    [1..999] |> List.filter(fun x -> x % 3 = 0 || x % 5 = 0 ) |> List.sum
+
+let Problem2 :int = 
+    let rec FibList (acc1, acc2) = [
+        let v = acc1 + acc2
+        if 4_000_000 < v then ()
+        else
+            yield v
+            yield! FibList(acc2, v)
+    ]
+
+    FibList (0, 1)
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.sum
