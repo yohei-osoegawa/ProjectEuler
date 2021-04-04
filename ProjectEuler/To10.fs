b395f9d5e1ba3b1dc06c0f156d6a573230863f08
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-04 14:28:46 +0900

    Problem5

--- ProjectEuler/To10.fs
+++ ProjectEuler/To10.fs
@@ -1,6 +1,7 @@
 ﻿module To10
 
 open Microsoft.FSharp.Core.Operators.Checked
+open System.Linq
 
 let Problem1 :int =
     [1..999] |> List.filter(fun x -> x % 3 = 0 || x % 5 = 0 ) |> List.sum
@@ -47,4 +48,25 @@ let Problem4 =
     |> fun (x,y) -> x*y
 
     
-        
+let Problem5 =
+    //2から20まで素因数分解を行って、それらを組み合わせる
+    let rec pf num div =
+        if ((float num) |> sqrt  |> int) < div then [num]
+        elif num % div = 0 then div :: pf (num / div) div
+        else pf num (div + 1)
+
+    let allPfList = 
+        [2..20] |> List.map (fun x -> pf x 2)
+        |> List.map (fun x -> x|> List.countBy id)
+        |> List.toArray
+        |> Array.reduce (fun acc x -> acc @ x)
+
+    let toDict =
+        let mutable map = new System.Collections.Generic.Dictionary<_,_>()
+        for (num, count) in allPfList do
+            match map.TryGetValue(num) with
+            | (true, count2) -> map.[num] <- max count count2
+            | (false, _) -> map.Add(num,count)
+        map
+
+    toDict.Aggregate( 1,fun  acc pair -> acc * (pown pair.Key pair.Value))
