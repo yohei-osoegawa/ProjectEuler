c60a06e5991d63951edf30186fd9a96df17cbb62
knockonthedoorintosummer <knockonthedoorintosummer@gmail.com>
2021-04-20 07:28:11 +0900

    Problem12 回答

--- ProjectEuler/To10.fs
+++ ProjectEuler/To10.fs
@@ -169,8 +169,11 @@ let Problem11 =
     ] |> List.map(fun [x1;x2;x3;x4] -> x1*x2*x3*x4)
     |> List.max
 
+/// 約数の個数が５００を超える最初の三角数
 let Problem12 =
-    // 三角数は(n+1)C2で表される。また約数の個数は素因数分解から求められる
+    // 方針：約数の個数を求めてそれが500より大きいか判断する関数を別途定義し、find関数使って、trueの時の最初の値を取得する
+
+    // Note:三角数は(n+1)C2で表される。また約数の個数は素因数分解から求められる
     // ここはメモ化で高速化の余地がある
     let rec pfd n i = 
         let max = n |> float |> sqrt |> int
@@ -178,18 +181,26 @@ let Problem12 =
         elif n % i = 0 then i :: pfd (n/i) i
         else pfd (n) (i+1)
 
+    let doesAnsPair (n, m) =
+        [n; m]
+        |> List.map (fun i -> pfd i (2))
+        |> List. collect id
+        |> List.countBy id
+        |> List.map (fun (x, count) -> if x = 2 then count - 1 else count)
+        |> List.fold (fun acc next -> acc * (next + 1)) (1)
+        |> (<) 500 
+
     // ここは表記方法に改善の余地あるかも
     seq{
         let mutable i = 2
         while true do
             yield i
             i<-i+1
-    } |> Seq.map (fun i -> pfd i (2))
+    } 
     |> Seq.pairwise
-    |> Seq.map ((fun (x1, x2) -> [x1 ; x2]) >> List. collect id)
-    |> Seq.map (List.countBy id) // countBy id も遅い、Dictionaryを使って手続き型で書くと早いはず
-    |> Seq.map (List.map (fun (x, count) -> if x = 2 then count - 1 else count) ) // ここで2で割ることも実施
-    |> Seq.map (List.fold (fun acc next -> acc * (next + 1)) (1) )
-    |> Seq.find ((<) 500)
+    |> Seq.find doesAnsPair
+    |> fun (n1, n2) -> n1 * n2 / 2 
+
+
 
 
